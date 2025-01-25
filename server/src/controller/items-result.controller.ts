59d705930ce0c018b86975a2aaf564476a99824d
fmolinadev <franciscomolina.dev@gmail.com>
2025-01-25 15:27:09 -0300

    feat: add controlleres for request

---
+++ server/src/controller/items-result.controller.ts
@@ -0,0 +1,28 @@
+/**
+ * @author Francisco Molina <franciscomolina.dev@gmail.com>
+ */
+
+import { Request, Response } from "express";
+import { getItemsResultService } from "../services";
+
+const getSearchItemsController = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const product = req.query.q;
+
+    if (!product) {
+      res.status(400).json({ error: "No se ingreso el nombre de producto" });
+      return;
+    }
+
+    const results = await getItemsResultService(product.toString());
+    res.status(200).json(results);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+};
+
+export default getSearchItemsController;
