c9517eaea03abaa54a9a0b0c21822755db4b1ad4
fmolinadev <franciscomolina.dev@gmail.com>
2025-01-25 15:25:51 -0300

    eat: add interfaces from requests

--- server/src/config/adapters/item-details/item-details.adapter.ts
+++ server/src/config/adapters/item-details/item-details.adapter.ts
@@ -1,5 +1,11 @@
-import { AxiosAdapter } from "../http";
+/**
+ * @author Francisco Molina <franciscomolina.dev@gmail.com>
+ */
 
-const UrlItemDetails = process.env.SEARCH_ITEM_API;
+import { AxiosAdapter } from "../http";
+import dotenv from "dotenv";
+dotenv.config();
+
+const UrlItemDetails = process.env.VIEW_ITEM_API;
 
 export const itemFetcher = new AxiosAdapter(`${UrlItemDetails}`);
--- server/src/config/adapters/result/result.adapter.ts
+++ server/src/config/adapters/result/result.adapter.ts
@@ -1,4 +1,10 @@
+/**
+ * @author Francisco Molina <franciscomolina.dev@gmail.com>
+ */
+
 import { AxiosAdapter } from "../http";
+import dotenv from "dotenv";
+dotenv.config();
 
 const UrlResults = process.env.SEARCH_ITEM_API;
 
