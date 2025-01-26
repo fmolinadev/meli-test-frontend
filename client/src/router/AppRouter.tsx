522729b5e0101b0ea7967b32386df41a9dc32c98
fmolinadev <franciscomolina.dev@gmail.com>
2025-01-25 21:25:38 -0300

    feat: define base routes and pages

--- client/src/router/AppRouter.tsx
+++ client/src/router/AppRouter.tsx
@@ -1,12 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import { ErrorPage, HomePage, ItemDetail, ItemsResults } from "../pages";
+import { AppLayout } from "../layout/AppLayout";
+
+const LayoutWrapper = () => (
+  <AppLayout>
+    <Outlet />
+  </AppLayout>
+);
 
 export function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/items" element={<ItemsResults />} />
-      <Route path="/items/:id" element={<ItemDetail />} />
+      <Route element={<LayoutWrapper />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/items" element={<ItemsResults />} />
+        <Route path="/items/:id" element={<ItemDetail />} />
+      </Route>
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
