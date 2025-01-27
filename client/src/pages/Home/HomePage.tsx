cf5c6aa7088f452e55632e696f65a566948b5611
fmolinadev <franciscomolina.dev@gmail.com>
2025-01-27 08:03:45 -0300

    feat: add favourites from details

--- client/src/pages/Home/HomePage.tsx
+++ client/src/pages/Home/HomePage.tsx
@@ -1,13 +1,21 @@
-import { MercadoPlay, ShippingBox, Slider } from "../../presentation";
-import styles from "./home.module.scss";
-
+import { Link } from "react-router-dom";
+import {
+  FavouriteItem,
+  MercadoPlay,
+  ShippingBox,
+  Slider,
+} from "../../presentation";
+import { useFavouritesContext } from "../../context";
 import sliderImage1 from "../../assets/images/slider/slider-0.png";
 import sliderImage2 from "../../assets/images/slider/slider-1.png";
 import sliderImage3 from "../../assets/images/slider/slider-2.png";
 import sliderImage4 from "../../assets/images/slider/slider-3.png";
 import sliderImage5 from "../../assets/images/slider/slider-4.png";
+import styles from "./home.module.scss";
 
 export const HomePage = () => {
+  const { favourites } = useFavouritesContext();
+
   const sliderImages = [
     sliderImage1,
     sliderImage2,
@@ -26,7 +34,22 @@ export const HomePage = () => {
         ))}
       </Slider>
       <div className={styles["center-home-placement"]}>
-        {/* //TODO: Favoritos aca */}
+        {favourites.length > 0 ? (
+          <div className={styles["favourites-container"]}>
+            {favourites.slice(0, 5).map((item) => (
+              <Link
+                key={item.id}
+                className={styles["link-no-style"]}
+                to={`/items/${item.id}`}
+              >
+                <div className={styles["favourite-item"]}>
+                  <FavouriteItem item={item} />
+                </div>
+              </Link>
+            ))}
+          </div>
+        ) : null}
+
         <h3 className={styles["section-title"]}>¿Qué es Mercado Play?</h3>
         <MercadoPlay />
         <ShippingBox />
